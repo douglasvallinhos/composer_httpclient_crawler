0df77100dd416f42b66c052cc6061be681b4d6c6
Douglas Vallinhos <douglas.m.vallinhos@gmail.com>
2020-03-31 21:55:26 -0300

    Primeiro Commit

---
+++ buscar-cursos.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Alura\BuscadorDeCursos\Buscador;
+
+$client = new Client(
+    [
+        'verify' => false,
+        'base_uri' => 'https://www.alura.com.br/'
+    ]
+);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client,$crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso){
+   echo $curso.PHP_EOL;
+}
